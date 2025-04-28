2d2242bcddb33bcec61034d5fb923d8d82c9ce03
iysaofficial <110337759+iysaofficial@users.noreply.github.com>
2025-04-28 13:29:13 +0700

    Update Certificatesupervisor.js

--- src/pages/certife/Certificatesupervisor.js
+++ src/pages/certife/Certificatesupervisor.js
@@ -29,6 +29,27 @@ const Certificatesupervisor = () => {
 
           <div className="px-12" data-aos="fade-down" data-aos-delay="600">
             <div className="grid sm:grid-cols-2 lg:grid-cols-5 gap-5">
+            <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+                <div className="m-2 text-justify text-sm">
+                  {/* <img
+                    alt="card img"
+                    className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out"
+                    src="./assets/logo/new.jpg"
+                  /> */}
+                  <h2 className="font-semibold my-4 text-2xl text-center">
+                    Sertifikat Supervisor 2025
+                  </h2>
+                  <a className="font-bold">
+                    {/* Lokasi Venue: {" "}   */}
+                    <a
+                      href="https://drive.google.com/drive/u/0/folders/1s0t2MxfwAlwTM_dIWrtj8oim6Qn7GCF7"
+                      target="_blank"
+                    >
+                      Klik Disini
+                    </a>
+                  </a>
+                </div>
+              </div>
               <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
                 <div className="m-2 text-justify text-sm">
                   {/* <img
