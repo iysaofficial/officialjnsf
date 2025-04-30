e9333aec3d13fc9b8e3cd128f91e17ec2378cb9d
young <youngscientist.iysa@gmail.com>
2025-04-30 08:38:49 +0700

    new

--- src/components/Aftermovie.js
+++ src/components/Aftermovie.js
@@ -3,7 +3,6 @@ import NavBar from "../components/Navbar/NavBar";
 import Footer from "../components/Footer";
 import { useDocTitle } from "../components/CustomHook";
 import axios from "axios";
-// import emailjs from 'emailjs-com';
 import Notiflix from "notiflix";
 
 const Aftermovie = () => {
