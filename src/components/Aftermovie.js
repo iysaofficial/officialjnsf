6e4dbc275dcc2858e5c3a81aad59a541f6e19579
young <youngscientist.iysa@gmail.com>
2025-05-02 10:14:49 +0700

    new

--- src/components/Aftermovie.js
+++ src/components/Aftermovie.js
@@ -84,11 +84,11 @@ const Aftermovie = () => {
             <div className="w-full bg-white p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
               <div className="flex">
                 <h1 className="font-bold text-center lg:text-left text-blue-900 uppercase text-4xl">
-                  After Movie 2024
+                  After Movie 2025
                 </h1>
               </div>
               <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
-                <iframe src="https://www.youtube.com/embed/kN7vJh84Zqk?si=aQoo-k0IWt_XuFFO"></iframe>
+                <iframe src="https://www.youtube.com/embed/DI6GevLONso?si=f-qedf1E82ht34hf"></iframe>
                 {/* <div>
                   <input
                     name="first_name"
