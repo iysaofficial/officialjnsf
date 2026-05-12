7390ff8c220bc6ee79952db805df0393da57b58d
iysaofficial <youngscientist.iysa@gmail.com>
2026-05-12 15:35:04 +0700

    after movie

--- src/components/Aftermovie.js
+++ src/components/Aftermovie.js
@@ -18,13 +18,13 @@ const Aftermovie = () => {
             <div className="w-full bg-white p-8 my-4 md:px-12 lg:w-9/12 lg:pl-20 lg:pr-40 mr-auto rounded-2xl shadow-2xl">
               <div className="flex">
                 <h1 className="font-bold text-center lg:text-left text-blue-900 uppercase text-4xl">
-                  After Movie 2025
+                  After Movie 2026
                 </h1>
               </div>
               <div className="grid grid-cols-1 gap-5 md:grid-cols-2 mt-5">
                 <iframe
                   title="After Movie JNSF 2025"
-                  src="https://www.youtube.com/embed/DI6GevLONso?si=f-qedf1E82ht34hf"
+                  src="https://youtu.be/tL4wsUFsikM?si=F5XYU_lXfJ2BoGCr"
                 ></iframe>
                 {/* <div>
                   <input
