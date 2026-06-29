8bdcd5cc9843c226e29bf06a34f6dc1530f73783
IYSA <youngscientist.iysa@gmail.com>
2026-06-29 09:05:42 +0700

    UP Kurasi 2026

--- src/pages/Kurasi/Kurasipage.js
+++ src/pages/Kurasi/Kurasipage.js
@@ -70,6 +70,28 @@ const Kurasipage = () => {
                   </p>
                 </div>
               </div>
+              <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
+                <div className="m-2 text-justify text-sm">
+                  {/* <img
+                    alt="card img"
+                    className="rounded-t group-hover:scale-[1.15] transition duration-1000 ease-in-out"
+                    src="./assets/logo/env.png"
+                  /> */}
+                  <h2 className="font-semibold my-4 text-2xl text-center ">
+                    Kurasi 2026
+                  </h2>
+                  <p className="font-bold">
+                    {/* Lokasi Venue: {" "}   */}
+                    <a
+                      href="https://drive.google.com/drive/folders/1s3KOSSsywfj6ZvwkTGvyYFueS0e3MfNi?usp=sharing"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Klik Disini
+                    </a>
+                  </p>
+                </div>
+              </div>
 
               {/* <div className="bg-white transition-all ease-in-out duration-400  overflow-hidden text-gray-700 hover:bg-gray-500 hover:text-white rounded-lg shadow-2xl p-3 group">
                 <div className="m-2 text-justify text-sm">
